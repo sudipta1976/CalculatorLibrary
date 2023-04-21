404a815fe4e4f9c02b1e94fe3c893cb99234fc81
sudipta1976 <su_sarkar@rediffmail.com>
2023-04-21 15:27:54 -0400

    fail workflow

--- calculator.py
+++ calculator.py
@@ -11,3 +11,7 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+# def multiply(first_term, second_term):
+#     return first_term * second_term
